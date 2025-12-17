eef61008261f654641a198b0db41dcfd2e3c1e38
Wan_en <1956757878@qq.com>
2025-12-17 18:00:16 +0800

    彻底修复 tm 编译错误

--- src/observer/common/type/char_type.cpp
+++ src/observer/common/type/char_type.cpp
@@ -1,3 +1,8 @@
+#include <time.h>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <locale>
 /* Copyright (c) 2021 OceanBase and/or its affiliates. All rights reserved.
 miniob is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
@@ -6,33 +11,20 @@ You may obtain a copy of Mulan PSL v2 at:
 THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-See the Mulan PSL v2 for more details. */
+See the Mulan PSL v2 for more details.
+*/
 
 #include "common/lang/comparator.h"
 #include "common/log/log.h"
 #include "common/type/char_type.h"
 #include "common/value.h"
-#include <time.h>
-
-int CharType::compare(const Value &left, const Value &right) const
-{
-  ASSERT(left.attr_type() == AttrType::CHARS && right.attr_type() == AttrType::CHARS, "invalid type");
-  return common::compare_string(
-      (void *)left.value_.pointer_value_, left.length_, (void *)right.value_.pointer_value_, right.length_);
-}
-
-RC CharType::set_value_from_str(Value &val, const string &data) const
-{
-  val.set_string(data.c_str());
-  return RC::SUCCESS;
-}
 
 RC CharType::cast_to(const Value &val, AttrType type, Value &result) const
 {
   switch (type) {
     case AttrType::DATES: {
         RC  rc = RC::SUCCESS;
-        std::tm tm = {};        
+        tm tm = {};
         std::istringstream iss(val.get_string());
         iss.imbue(std::locale("zh_CN.utf-8"));
         iss >> std::get_time(&tm, "%Y-%m-%d");
@@ -52,21 +44,5 @@ RC CharType::cast_to(const Value &val, AttrType type, Value &result) const
       break;
     default: return RC::UNIMPLEMENTED;
   }
-  return RC::SUCCESS;
-}
-
-int CharType::cast_cost(AttrType type)
-{
-  if (type == AttrType::CHARS) {
-    return 0;
-  }
-  return INT32_MAX;
-}
-
-RC CharType::to_string(const Value &val, string &result) const
-{
-  stringstream ss;
-  ss << val.value_.pointer_value_;
-  result = ss.str();
   return RC::SUCCESS;
 }
